d575e9c41d53a04fafd237fbceb02b8277d9e496
Chris <chris@christophergreening.com>
2022-07-02 17:07:25 -0400

    ADD: added final bit of code

--- posts/Performing multiple joins in R using dplyr and purrr/code/process_data.R
+++ posts/Performing multiple joins in R using dplyr and purrr/code/process_data.R
@@ -8,11 +8,6 @@ library(dplyr)
 .process.data <- function(dataset, eu.agricultural.codes, eu.country.codes) {
   return(
     dataset %>%
-      dplyr::select(geo, TIME_PERIOD, animals, OBS_VALUE) %>%
-      dplyr::mutate(
-        animals = eu.agricultural.codes[animals],
-        geo = eu.country.codes[geo]
-      ) %>%
-      tidyr::pivot_wider(names_from = animals, values_from = OBS_VALUE)
+      
   )
 }
--- posts/Performing multiple joins in R using dplyr and purrr/code/sample_code.R
+++ posts/Performing multiple joins in R using dplyr and purrr/code/sample_code.R
@@ -1,16 +1,16 @@
 # Author: Chris Greening
 # Date: 2022-07-02
-# Purpose: Sample code for cleaning and joining datasets using the tidyverse
+# Purpose: Sample code for cleaning and joining datasets using tidyverse libraries
 
 library(readr)
 library(purrr)
 library(dplyr)
 library(tibble)
+library(tidyr)
 
 setwd("C:/Users/chris/OneDrive - christophergreening.com/Documents/Programming/chris-greening-blog/posts/Performing multiple joins in R using dplyr and purrr/code")
-source("process_data.R")
 
-#### Load code maps ----
+#### Read lookup tables and deframe into named vectors ----
 eu.agricultural.codes <- readr::read_csv("data/euro_agricultural_codes.csv") %>%
   select(Code, Description) %>%
   tibble::deframe()
@@ -18,11 +18,16 @@ eu.country.codes <- readr::read_csv("data/euro_country_codes.csv") %>%
   select(geo, country) %>%
   tibble::deframe()
 
-#### Data processed, pivoted wide, and outer joined 
+#### Load data, use lookup tables to convert codes to useful names, pivot data wide, and outer join 
 livestock.data <- list.files("data/eurostat_livestock_data/", full.names = TRUE) %>%
   purrr::map(function(fpath) {
     readr::read_csv(fpath) %>%
-      .process.data(eu.agricultural.codes, eu.country.codes)
+      dplyr::select(geo, TIME_PERIOD, animals, OBS_VALUE) %>%
+      dplyr::mutate(
+        animals = eu.agricultural.codes[animals],
+        geo = eu.country.codes[geo]
+      ) %>%
+      tidyr::pivot_wider(names_from = animals, values_from = OBS_VALUE)
   }) %>%
   purrr::reduce(~dplyr::full_join(.x, .y, by=c("geo", "TIME_PERIOD")))
 
